32fac24d281d1cbfb8388aa8da9a6af64a6db525
Sayan220 <122210469+Sayan220@users.noreply.github.com>
2023-04-08 15:34:42 +0530

    Update linked.c

--- linked.c
+++ linked.c
@@ -57,11 +57,58 @@ void end_insert(){
     }
     temp->next=newnode;
 }
+void beg_del(){              //deletion from beginning
+    temp=head;
+    head=head->next;
+    free(temp);
+}
+void end_del(){                   
+    struct node *prev;             //deletion from end
+    temp=head;
+    while(temp->next!=0){
+        prev=temp;
+        temp=temp->next;
+    }
+    if(temp==head){ head=0;}
+    else{  prev->next=0;}
+    free(temp);
+}
+void reverse(){
+    struct node *prev,*current,*nextnode;  //reverse the linked list
+    prev=0;
+    current=nextnode=head;
+    while(nextnode!=0){
+        nextnode=nextnode->next;
+        current->next=prev;
+        prev=current;
+        current=nextnode;
+    }
+    head=prev;
+}
+void sort(){
+    int t;
+    temp=head;
+    struct node *index=NULL;
+    while(temp != NULL) {
+        index=temp->next;           
+        while(index != NULL) {  
+             if(temp->data > index->data) {  
+             t=temp->data;  
+             temp->data = index->data;  
+             index->data = t;  
+            }  
+         index=index->next;  
+        }  
+        temp=temp->next; 
+    }
+}
+
 int main()
 {
     int choice;
     while(1){
-        printf("\nmenu....\nenter 1 for create\nenter 2 for display\nenter 3 to insert at the beginning\nenter 4 to insert at the end\nenter enter 0 for exit.\n\nenter choice: ");
+        printf("\nmenu....\nenter 1 for create\nenter 2 for display\nenter 3 to insert at the beginning\nenter 4 to insert at the end\nenter 5 to delete at start\nenter 6 to delete at end\n");
+        printf("enter 7 to reverse\nenter 8 to sort\nenter enter 0 for exit.\n\nenter choice: ");
         scanf("%d",&choice);
         switch(choice){
             case 1: create();
@@ -71,7 +118,15 @@ int main()
             case 3: beg_insert();
               break;
             case 4: end_insert();
-              break;     
+              break;
+            case 5: beg_del();
+              break;
+            case 6: end_del();
+              break;
+            case 7: reverse();
+              break;
+            case 8: sort();
+              break;             
             case 0: printf("\nexit...."); 
               exit(0);
             default: printf("\nwrong choice. Try again.\n");
@@ -80,3 +135,4 @@ int main()
     }
     return 0;
 }
+
