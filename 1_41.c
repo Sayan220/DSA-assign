7f66ee31c20816d797f06bc00eb90c8213428150
Sayan220 <122210469+Sayan220@users.noreply.github.com>
2023-04-10 22:40:27 +0530

    Update 1_41.c

--- 1_41.c
+++ 1_41.c
@@ -13,7 +13,7 @@ int* create(int *a){
 	return a;
 }
 
-void display(int *a){
+void display(int *a, int size){
 	int i;
 	printf("\nthe array :  [ ");
 	for(i=0;i<size;i++)
@@ -81,50 +81,123 @@ void search(int *a,int ele){
 	     printf("\nelement not found");
 }   	
 
-void insert(int m,int k,int *a){
-	int i;
+void insert(int item,int k,int *a){
 	a=realloc(a,sizeof(int)*(size+1));
 	size++;
-	for(i=size-1;i>=k;i--){
+	for(int i=size-1;i>=k;i--){
 		a[i]=a[i-1];
 	 }
-	a[k]=m;
-	display(a);
+	a[k]=item;
 }
 
-void merge(int **arr1, int **arr2)
-{
-    // Allocate memory for new array
-    int *new_arr = (int *) malloc((size + size) * sizeof(int));
-    // Copy elements from array 1
-    int i;
-    for (i = 0; i < size; i++) {
-        new_arr[i] = (*arr1)[i];
-    }
-    // Copy elements from array 2
-    for (i = 0; i < size; i++) {
-        new_arr[size+i] = (*arr2)[i];
-    }
-	//printf("after for loop");
-    // Update array and size
-    //free(*arr1);
-    *arr1 = new_arr;
-    *n1 += *n2;
+void delete(int k,int*a){
+	if(k<0 || k>=size){
+		printf("\ninvalid position");
+		return;
+	}
+	else{
+		for(int i=k;i<size-1;i++){
+			a[i]=a[i+1];
+		}
+	}
+	size--;
 }
+
+void merge(int *a,int *b){
+	int size2;
+	int *res=NULL;
+	printf("\nenter the size of  2nd array: ");
+	scanf("%d",&size2);
+	b=(int*)malloc(size2*sizeof(int));
+	printf("\nenter the elements:");
+	for(int i=0;i<size2;i++){
+		scanf("%d",&b[i]);
+	}
+	int i=0,j=0,k=0;
+	res=(int*)malloc((size+size2)*sizeof(int));
+	while(i<size && j<size2){
+		if(a[i]<b[j]){
+			res[k]=a[i];
+			i++; k++;
+		}
+		else{
+			res[k]=b[j];
+			j++; k++;
+		}
+	}
+	while(i<size){
+		res[k++]=a[i++];
+	}
+	while(j<size2){
+		res[k++]=b[j++];
+	}
+	display(res,k);
+}
+
+/*void split(int **a, int *n1, int **a2, int *n2, int pos)
+{
+    *a2 = (int *) malloc((*n1 - pos)*sizeof(int));
+    int i;
+    for (i = pos; i < *n1; i++) {
+        (*a2)[i-pos] = (*a)[i];
+    }
+    // Resize array 1
+    *a = (int *) realloc(*a, pos * sizeof(int));
+    *n2 = *n1 - pos;
+    *n1 = pos;
+}*/
+void split(int *a, int size)
+{
+    int *s1,*s2,i,k;
+    if(a==NULL){
+    	return;
+	}
+	s1=(int*)malloc(sizeof(int)*(size/2));
+	s2=(int*)malloc(sizeof(int)*(size/2));
+	for(i=0;i<size/2;i++){
+		s1[i]=a[i];
+	}
+	k=0;
+	for(;i<size;i++){
+		s2[k++]=a[i];
+	}
+	printf("\n1st half\n");
+	display(s1,size/2);
+	printf("\n2nd half\n");
+	display(s2,k);
+}
+
+void index_of(int *a,int ele){
+	int i,found=0;
+	printf("Indices of %d:",ele);
+	for(i=0;i<size;i++)
+	{
+		if(a[i]==ele)
+		{
+			printf("%d ",i);
+			found=1;
+		}
+	}
+	if(!found)
+		printf("None");
+	
+	printf("\n");
+}   	
 
 int main(){
 	int *a=NULL;
-	int *b=NULL
-	int ch,l,m,k;
+	int *b=NULL;
+	int ch,l,item,pos;
 	while(1){
 		  printf("\n\nMENU.....");
-		  printf("\nenter 1 for create \nenter 2 for display \nenter 3 for count\nenter 4 for reverse\nenter 5 for sort\nenter 6 for indexed element\nenter 7 to search\nenter 8 to insert\nenter 10 to exit....\n\nenter choice:  ");
+		  printf("\nenter 1 for create \nenter 2 for display \nenter 3 for count\nenter 4 for reverse\nenter 5 for sort\nenter 6 for indexed element\nenter 7 to search\n");
+		  printf("enter 8 to insert\nenter 9 to delete\nenter 10 to merge\nenter 11 to split\nenter 12 to find index of an element\nenter 0 to exit....\n\nenter choice:  ");
 		  scanf("%d",&ch);
 		  switch(ch)
 		  {
 		    case 1: a=create(a);
 			   break;
-		    case 2: display(a);
+		    case 2: display(a,size);
 			   break;
 		    case 3: count();
 			   break;
@@ -134,24 +207,32 @@ int main(){
 			   break;
 		    case 6: printf("\nenter an index ");
 			    scanf("%d",&l);
-			    indexed_element(a,l);
+			    indexed_element(a,l);  // for finding element at a given index
 			    break;
 		    case 7: printf("\nenter the element to be search ");
 			    scanf("%d",&l);
 			    search(a,l);
 			    break;
 		    case 8: printf("\nenter the elemnet:");
-			    scanf("%d",&m);
-			    printf("\nenter the position:");
-			    scanf("%d",&k);
-			    insert(m,k,a);
+			    scanf("%d",&item);
+			    printf("\nenter the position:(0 indexing)  ");
+			    scanf("%d",&pos);
+			    insert(item,pos,a);
 			    break;
-			case 9: printf("create 2nd array : ");
-				b=create(b);
-				//printf("break");
-				merge(&a,&b);
-				break;	
-		    case 10: printf("\n..........exit.........."); exit(0);
+			case 9:	printf("\nenter the position:(0 indexing)  ");
+			    scanf("%d",&pos);
+			    delete(pos,a);
+				break;
+			case 10: printf("create 2nd array :\n ");
+				merge(a,b);
+				break;
+			case 11: split(a,size);
+				break;
+			case 12: printf("\nenter the element to be search ");      //for finding index of a given element
+			    scanf("%d",&l);
+			    index_of(a,l);
+				break;			 
+		    case 0: printf("\n..........exit.........."); exit(0);
 
 		    default : printf("\nwrong choice. try again");
 			     break;
@@ -159,5 +240,3 @@ int main(){
 		}
 	return 0;
 }
-
-	
